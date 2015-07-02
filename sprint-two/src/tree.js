83a37693d08af8814a6c3c31da8a4e835d5848a7
MKS-students <mks.students@hackreactor.com>
2015-07-02 11:59:04 -0700

    Implements tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
@@ -15,10 +17,41 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  var newChild = Tree(value);
+  this.children.push(newChild);
 
 };
 
+// treeMethods.contains = function(target){
+//   var currentNode;
+//     if (!arguments[1]) {
+//       currentNode=this;
+//     } else {
+//       currentNode = arguments[1];
+//     }
+//   // console.log("currentNode is ", currentNode);
+
+//   if (currentNode.value===target) { return true; }
+//   for (var i = 0; i<currentNode.children.length; i++){
+//     console.log("currentNode children i is ", currentNode.children[i]);
+//     return currentNode.contains(target, currentNode.children[i]);
+//   }
+//   // console.log("currentNode at bottom is ", currentNode);
+//   return false;
+
+// };
+
 treeMethods.contains = function(target){
+  var wasFound = false;
+
+  var test = function(target, node){
+    if (node.value===target) { wasFound = true; }
+    for (var i = 0; i<node.children.length; i++){
+      test(target, node.children[i]);
+    }
+  };
+  test(target, this)
+  return wasFound;
 
 };
 
