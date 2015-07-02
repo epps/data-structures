20df708e939a9490b799c8c83f032496ac2fb3fd
MKS-students <mks.students@hackreactor.com>
2015-07-01 17:32:07 -0700

    just started linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,19 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    // if list.head is null, then list.head equals this newest node
+    // list.tail equals this newest node
+    // previous node.next equals this newest node
+    // newest node.value to equal the value
   };
 
   list.removeHead = function(){
+    // list.head equals node after list.head
+      // delete the previous head (?)
   };
 
   list.contains = function(target){
+    // looks through all nodes and returns true if target value exists in a child
   };
 
   return list;
